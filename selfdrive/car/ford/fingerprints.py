30bb0e30a28321c4e018c19a4f4c8442b5092624
gustavoclimaco <143465602+gustavoclimaco@users.noreply.github.com>
2024-12-25 21:04:47 -0300

    Update_EDGE_Fusion.py

--- selfdrive/car/ford/fingerprints.py
+++ selfdrive/car/ford/fingerprints.py
@@ -49,16 +49,21 @@ FW_VERSIONS = {
   CAR.FORD_EDGE_MK2: {
     (Ecu.eps, 0x730, None): [
       b'M2GC-14D003-AA\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'K2GC-14D003-AJ\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', #STRATEGY_FUSION_HYBRID_AWD2017_PSCM
     ],
     (Ecu.abs, 0x760, None): [
       b'M2GC-2D053-CB\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
       b'M2GC-2D053-EA\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'HG9C-2D053-AH\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', #STRATEGY_FUSION_HYBRID_ABS
+      b'HG9C-2D053-MG\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', #STRATEGY_FUSION_AWD2017_ABS
     ],
     (Ecu.fwdRadar, 0x764, None): [
       b'JX7T-14D049-AD\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'LB5T-14D049-AB\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.fwdCamera, 0x706, None): [
       b'KT4T-14F397-AF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'KT4T-14F397-AE\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', #STRATEGY_FUSION_HYBRID_AWD2017_CAMERA
     ],
   },
   CAR.FORD_ESCAPE_MK4_23REFRESH: {
